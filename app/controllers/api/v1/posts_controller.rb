b2a389df8f88cd6de70dade1f7049ba6ef44b5a8
mattenbar <matthw.enbar@gmail.com>
2021-03-05 09:49:50 -0500

    CRUD acctions created for posts

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -10,4 +10,36 @@ class Api::V1::PostsController < ApplicationController
     render json: {post: PostSerializer.new(post)}
   end
 
+  def create
+    post = Post.new(post_params)
+    if post.save
+      render json: {post: PostSerializer.new(post)}
+    else
+      render json: {error: 'Unable to creat post'}
+    end
+
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    posts = Post.all
+    render json: {post: PostSerializer.new(posts)}
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    posts = Post.all
+    render json: {post: PostSerializer.new(posts)}
+
+  end
+
+  private
+
+
+  def post_params
+    params.require(:post).permit(:id, :category_id, :title, :content, :author)
+  end
+
 end

890119d4bd47c0a6e85daac80eff56d17bdbae79
mattenbar <matthw.enbar@gmail.com>
2021-03-05 09:32:47 -0500

    created show route for posts

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -4,4 +4,10 @@ class Api::V1::PostsController < ApplicationController
     posts = Post.all
     render json: {post: PostSerializer.new(posts)}
   end
+
+  def show
+    post = Post.find(params[:id])
+    render json: {post: PostSerializer.new(post)}
+  end
+
 end

ba5d1df39ee80d51aa05653137ae838a7236f7e7
osha7 <ogroetz@gmail.com>
2021-03-27 12:50:47 -0600

    backend for edit and deleting posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_22_142646) do
+ActiveRecord::Schema.define(version: 2021_03_26_233155) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2021_03_22_142646) do
     t.string "company_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "features", "posts"

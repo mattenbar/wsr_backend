9e65519b8f4de92fef3f2a4236c1f8dd3ff822ac
mattenbar <matthw.enbar@gmail.com>
2021-04-06 09:37:08 -0400

    fixing routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,42 +4,40 @@ Rails.application.routes.draw do
   
   root to: 'application#home'
 
-  
+  resources :ads
 
-      resources :ads
-
-      resources :pointcps do
-        resources :pointcpvotes
-      end
-      resources :pointcpvotes
-
-      resources :features do
-        resources :posts
-      end
-
-      resources :showads do
-          resources :ads
-      end
-      
-      resources :categories do
-        resources :posts
-      end
-      
-      resources :posts do 
-        resources :postvotes
-      end
-      
-      resources :postvotes
-
-      resource :users, only: [:index, :create]
-      post "/login", to: "auth#login"
-      get "/auto_login", to: "auth#auto_login"
-      get "/user_is_authorized", to: "auth#user_is_authorized"
-
-      put "/image_upload", to: "posts#image_upload"
-      put "/image_upload_pointcp", to: "pointcps#image_upload"
-      put "/image_upload_ads", to: "ads#image_upload"
-      
+  resources :pointcps do
+    resources :pointcpvotes
+  end
     
+  resources :pointcpvotes
 
+  resources :features do
+    resources :posts
+  end
+
+  resources :showads do
+    resources :ads
+  end
+      
+  resources :categories do
+    resources :posts
+  end
+      
+  resources :posts do 
+    resources :postvotes
+  end
+      
+  resources :postvotes
+
+  resource :users
+
+  post "/login", to: "auth#login"
+  get "/auto_login", to: "auth#auto_login"
+  get "/user_is_authorized", to: "auth#user_is_authorized"
+
+  put "/image_upload", to: "posts#image_upload"
+    put "/image_upload_pointcp", to: "pointcps#image_upload"
+  put "/image_upload_ads", to: "ads#image_upload"  
+  
 end
